a0f59fbc4d01dee40fbd37f1d106e548d06031bc
George Kaplan <george@georgekaplan.xyz>
2019-06-18 22:08:56 -0400

    try reloading trick

--- build.sh
+++ build.sh
@@ -6,3 +6,5 @@ docker run \
        --mount src=$PWD,target=/usr/src/tex,type=bind \
        dxjoke/tectonic-docker \
        /bin/sh -c "tectonic -o build Kaplan_George_resume.tex"
+
+cp index.html build
